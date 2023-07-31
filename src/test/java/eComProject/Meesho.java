2b5aea8a7fa8c56b055c638ace8cb2932e994496
Bhuwaneshwari <bhuwaneshwari14@gmail.com>
2023-07-31 15:32:34 +0530

    [Bhuwaneshwari]Adding another code

---
+++ src/test/java/eComProject/Meesho.java
@@ -0,0 +1,18 @@
+package eComProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Meesho {
+  @Test
+  public void meeshoTest() throws InterruptedException {
+	  WebDriver driver= new ChromeDriver();
+	  Reporter.log("Opening Meesho", true);
+	  driver.get("https://www.meesho.com/");
+	  driver.manage().deleteAllCookies();
+	  Thread.sleep(5000);
+	  driver.close();
+  }
+}
